7e753d16ea98e567c4c9b976fe07f9669fa012e4
salonikumari241055@gmail.com <salonikumari241055@gmail.com>
2025-12-17 14:42:52 +0530

    push my first program

--- firstprogram.c
+++ firstprogram.c
@@ -1,8 +1,48 @@
 #include<stdio.h>
+int multiply()
+{
+
+   int num;
+   int first_number=30;
+   int second_number=40;
+   printf("\nmultiply: %d ",first_number*second_number); 
+}
+
+int divide()
+{
+
+    int first_number= 50;
+    int second_number=40;
+    printf("\ndivide: %d ",first_number/second_number);
+}
 int main()
 {
-    printf("saloni");
+    int num;
+    printf("\n1.multiply");
+    printf("\n2.divide");
+    printf("\nenter a any number");
+    scanf("%d",&num);
+
+    if(num==1)
+    {
+         multiply();
+
+    }
+     else if(num==2)
+    {
+        divide();
+    }
+
+
    
    
-    return 0;
-}
+
+   return 0;
+   
+   }
+
+
+
+
+
+
